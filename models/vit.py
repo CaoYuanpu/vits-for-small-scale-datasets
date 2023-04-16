91aa8637da1d2f8706dd93cd94f3b61464d67dd8
CaoYuanpu <yuanpucao@gmail.com>
2023-04-16 12:19:46 -0400

    lora

--- models/vit.py
+++ models/vit.py
@@ -121,7 +121,7 @@ class Attention_split(nn.Module):
         q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
         k = self.k(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
         v = self.v(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
-        print(q.shape, k.shape, v.shape)
+        print('split', q.shape, k.shape, v.shape)
         input()
 
         # qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
