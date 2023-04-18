af5b57e6641fc1c3a2bdae8e0a1a2c2cfc4a77ab
CaoYuanpu <yuanpucao@gmail.com>
2023-04-18 01:26:13 -0400

    lora

---
+++ rank8.sh
@@ -0,0 +1,4 @@
+python3 -u vit_lora_split_reset.py  --arch vit_lora_split --dataset CIFAR100 --datapath ~/data/cifar-100-python --lr 0.004 --batch_size 256 --epochs 200 --lora_rank 8 --lora_reset 10 --gpu 1 --weight-decay 0 --warmup 1
+
+
+python3 -u vit_lora_split_reset.py  --arch vit_lora_split --dataset CIFAR100 --datapath ~/data/cifar-100-python --lr 0.004 --batch_size 256 --epochs 200 --lora_rank 8 --lora_reset 250 --gpu 1 --weight-decay 0 --warmup 1
