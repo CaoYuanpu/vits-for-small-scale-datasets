50ea1768b9ff32aeb13087c213a222de766e3239
CaoYuanpu <yuanpucao@gmail.com>
2023-04-18 00:47:53 -0400

    lora

--- vit_lora_split_reset.py
+++ vit_lora_split_reset.py
@@ -28,6 +28,28 @@ import loralib as lora
 best_acc1 = 0
 MODELS = ['vit', 'vit_lora', 'vit_lora_split', 'swin' , 'cait']
 
+def mark_only_lora_as_trainable(model: nn.Module, bias: str = 'none') -> None:
+    for n, p in model.named_parameters():
+        if 'lora_' not in n:
+            p.requires_grad = False
+
+    for n, p in model.named_parameters():
+        if 'norm' in n:
+            p.requires_grad = True
+    if bias == 'none':
+        return
+    elif bias == 'all':
+        for n, p in model.named_parameters():
+            if 'bias' in n:
+                p.requires_grad = True
+    elif bias == 'lora_only':
+        for m in model.modules():
+            if isinstance(m, LoRALayer) and \
+                hasattr(m, 'bias') and \
+                m.bias is not None:
+                    m.bias.requires_grad = True
+    else:
+        raise NotImplementedError
 
 def init_parser():
     parser = argparse.ArgumentParser(description='Vit small datasets quick training script')
@@ -159,7 +181,7 @@ def main(args):
         logger.debug('label smoothing used')
         print('*'*80+Style.RESET_ALL)
         criterion = LabelSmoothingCrossEntropy()
-    
+
     else:
         criterion = nn.CrossEntropyLoss()    
         
@@ -251,7 +273,7 @@ def main(args):
         Training
     '''
 
-    lora.mark_only_lora_as_trainable(model)
+    mark_only_lora_as_trainable(model)
 
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
     
