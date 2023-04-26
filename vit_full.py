c1b6002de2c736042c1128ca7679fb3c8582d065
CaoYuanpu <yuanpucao@gmail.com>
2023-04-26 11:35:49 -0400

    lora

--- vit_full.py
+++ vit_full.py
@@ -332,6 +332,10 @@ def main(args):
         n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
         logger.debug(f'Number of params: {format(n_parameters, ",")}')
         input()
+        print(model.blocks.0.attn.q.weight[0][:10])
+        print(model.blocks.0.attn.q.lora_A[0][:10])
+        print(model.blocks.0.attn.q.lora_B[0][:])
+        input()
         lr = train(train_loader, model, criterion, optimizer, epoch, scheduler, args)
         acc1 = validate(val_loader, model, criterion, lr, args, epoch=epoch)
         torch.save({
