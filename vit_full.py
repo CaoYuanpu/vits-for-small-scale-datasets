7beb3afeba206736e0ddc4a69877e439ca3fb75c
CaoYuanpu <yuanpucao@gmail.com>
2023-04-26 11:38:18 -0400

    lora

--- vit_full.py
+++ vit_full.py
@@ -329,12 +329,10 @@ def main(args):
     for epoch in tqdm(range(args.epochs)):
         for n, p in model.named_parameters():
             print(n, p.shape, p.requires_grad)
+            if n == 'blocks.0.attn.q.weight' or n == 'blocks.0.attn.q.lora_A' or n == 'blocks.0.attn.q.lora_B':
+                print(p[0][:10])
         n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
         logger.debug(f'Number of params: {format(n_parameters, ",")}')
-        input()
-        print(model.blocks.0.attn.q.weight[0][:10])
-        print(model.blocks.0.attn.q.lora_A[0][:10])
-        print(model.blocks.0.attn.q.lora_B[0][:])
         input()
         lr = train(train_loader, model, criterion, optimizer, epoch, scheduler, args)
         acc1 = validate(val_loader, model, criterion, lr, args, epoch=epoch)
